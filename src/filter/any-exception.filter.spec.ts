c59b6c1b6d2dd675fa3186d1c5ee28afd08ee5eb
BoBoooooo <17746714@qq.com>
2020-08-10 15:09:15 +0800

    delete(test): 去掉单元测试相关spec

--- src/filter/any-exception.filter.spec.ts
+++
@@ -1,7 +0,0 @@
-import { AllExceptionsFilter } from './any-exception.filter';
-
-describe('AnyExceptionFilter', () => {
-  it('should be defined', () => {
-    expect(new AllExceptionsFilter()).toBeDefined();
-  });
-});
