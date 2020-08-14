8eea4bd18b552f681363b551897112c471c0b988
BoBoooooo <17746714@qq.com>
2020-08-14 10:35:16 +0800

    feat(jwtConfig): 调整jwt config目录结构

---
+++ src/config/constants.ts
@@ -0,0 +1,11 @@
+/*
+ * @file: 设置jwt相关参数
+ * @copyright: NanJing Anshare Tech .Com
+ * @author: BoBo
+ * @Date: 2020年08月06 16:10:49
+ */
+export const jwtConstants = {
+  secret: 'shinobi7414', // 秘钥
+  time:'24h'
+};
+
--- src/module/auth/auth.module.ts
+++ src/module/auth/auth.module.ts
@@ -1,22 +1,21 @@
-import { LocalStrategy } from './../../guards/local.strategy';
 import { JwtStrategy } from './../../guards/jwt.strategy';
 import { UsersModule } from '../users/users.module';
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
-import { jwtConstants } from 'src/guards/constants';
+import { jwtConstants } from 'src/config/constants';
 
 @Module({
   imports: [
     PassportModule.register({ defaultStrategy: 'jwt' }),
     JwtModule.register({
       secret: jwtConstants.secret,
-      signOptions: { expiresIn: '24h' }, // token 过期时效
+      signOptions: { expiresIn: jwtConstants.time }, // token 过期时效
     }),
     UsersModule,
   ],
-  providers: [AuthService,LocalStrategy, JwtStrategy,],
+  providers: [AuthService, JwtStrategy,],
   exports: [AuthService],
 })
 export class AuthModule {}
