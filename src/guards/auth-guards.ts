18569095f595d4791c3e71d5b95e60481581f0e7
BoBoooooo <17746714@qq.com>
2020-08-14 10:34:16 +0800

    feat(jwtGuard): 修改guard拦截逻辑,标注@NoAuth()即可跳过token验证

--- src/guards/auth-guards.ts
+++ src/guards/auth-guards.ts
@@ -1,3 +1,10 @@
+/*
+ * @file: 全局jwt guard
+          接口上添加@NoAuth() 则跳过校验
+ * @copyright: NanJing Anshare Tech .Com
+ * @author: BoBo
+ * @Date: 2020年08月07 14:53:52
+ */
 import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { Reflector } from '@nestjs/core';
@@ -6,17 +13,19 @@ import { AuthGuard, IAuthGuard } from '@nestjs/passport';
 @Injectable()
 export class RoleAuthGuard implements CanActivate {
   constructor(private readonly reflector: Reflector) {}
-  canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
-  //    在这里取metadata中的no-auth，得到的会是一个bool
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    // 在这里取metadata中的no-auth，得到的会是一个bool
     const noAuth = this.reflector.get<boolean>('no-auth', context.getHandler());
     const guard = RoleAuthGuard.getAuthGuard(noAuth);
-    return guard.canActivate(context);    //    执行所选策略Guard的canActivate方法
+    return typeof guard === 'boolean' ? guard : guard.canActivate(context); //执行所选策略Guard的canActivate方法
   }
 
-//    根据NoAuth的t/f选择合适的策略Guard
-  private static getAuthGuard(noAuth: boolean): IAuthGuard {
+  // @NoAuth() 则表示直接放行不校验token 
+  private static getAuthGuard(noAuth: boolean): IAuthGuard | boolean {
     if (noAuth) {
-      return new (AuthGuard('local'))();
+      return true;
     } else {
       return new (AuthGuard('jwt'))();
     }
