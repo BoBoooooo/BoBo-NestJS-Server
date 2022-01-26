9f8673e8af529b9c1add09b00a522f4485da7fe3
BoBoooooo <17746714@qq.com>
2022-01-26 17:13:09 +0800

    chore: change db password

--- src/config/env/test.ts
+++ src/config/env/test.ts
@@ -17,7 +17,7 @@ export default {
     host: 'localhost',
     port: 3306,
     username: 'root',
-    password: 'Anshare.cc',
+    password: '',
     database: 'nest',
     charset: 'utf8mb4',
     entities: [join(__dirname, '..', '..', '/entities/*{.ts,.js}')],
